b5efbf9e700c53adaa1aab324235d28effc64251
TheInsayn <mat.woess@gmail.com>
2019-12-11 16:56:37 +0100

    day04 part 2

--- challenges/day04.py
+++ challenges/day04.py
@@ -19,6 +19,19 @@ def test_conditions1(number):
     return six_digits and in_range and adjacent_digits and increasing
 
 
+def test_conditions2(number):
+    digits = [int(d) for d in str(number)]
+    six_digits = len(digits) == 6
+    in_range = 100000 <= number <= 999999
+    increasing = sorted(digits) == digits
+    pair_of_matching_digits = False
+    for i in range(0, 10):
+        count = sum([1 for d in digits if d == i])
+        if count == 2:
+            pair_of_matching_digits = True
+    return six_digits and in_range and pair_of_matching_digits and increasing
+
+
 class Day04(Challenge):
     input = range(246540, 787419 + 1)
 
@@ -30,4 +43,8 @@ class Day04(Challenge):
         self.print_result(len(result))
 
     def part2(self):
-        pass
+        result = list()
+        for i in self.input:
+            if test_conditions2(i):
+                result.append(i)
+        self.print_result(len(result))
